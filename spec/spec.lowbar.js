8ace5f9ffb74b66fc737dc49806d7908c8d36f4c
William Pickering <willpicky@gmail.com>
2017-03-20 22:57:31 +0000

    [_.identity] tests passing

--- spec/spec.lowbar.js
+++ spec/spec.lowbar.js
@@ -1,12 +1,23 @@
-var path = require('path');
-var expect = require('chai').expect;
-var _ = require(path.join(__dirname, '..', './lowbar.js'));
+const path = require('path');
+const expect = require('chai').expect;
+const _ = require(path.join(__dirname, '..', './lowbar.js'));
 
 describe('_', () => {
   'use strict';
 
-  it('is an object', function(){
+  it('is an object', () => {
     expect(_).to.be.an('object');
   });
-  
+
+  // INDENTITY
+  describe('#identity', () => {
+    it('is a function', () => {
+      expect(_.identity).to.be.a('function');
+    });
+    it('returns the arguments or 0', () => {
+      let actual = _.identity([12, 34, 21, 356]);
+      let expected = [12, 34, 21, 356];
+      expect(actual).to.eql(expected);
+    });
+  });
 });
